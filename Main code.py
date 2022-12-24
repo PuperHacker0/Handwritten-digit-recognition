6f467c7d133f829b4385bbfabe855b656606619f
PuperHacker0 <42037691+PuperHacker0@users.noreply.github.com>
2022-12-25 00:04:00 +0200

    Update Main code.py
    Finished creating the basic functions for the interface and model.
    Finished writing all pending functions to connect the former and the latter, program is now functional, but sometimes gives wrong results.

--- Main code.py
+++ Main code.py
@@ -1,10 +1,12 @@
 #Importing necessary files for graphics interface and model
+
+from PIL import Image, ImageDraw
 import tkinter as tk
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import os
 import cv2
 import numpy as np
@@ -12,87 +14,117 @@ import numpy as np
 class App():
     def __init__(self, root):
         self.root = root
-        self.root.geometry('800x600')
-        root.title("Digit recognition")
+        self.root.geometry('600x800')
+        self.root.title("Digit recognition")
+        self.create_label()
         self.create_canvas()
         self.create_buttons()
+        self.root.resizable(width = False, height = False)
+        self.image1 = Image.new("RGB", (600, 600), (255,255,255))
+        self.carbonCopy = ImageDraw.Draw(self.image1)
+        self.train_test_model()
 
     def ok_button_pushed(self): #Called when ok pressed
-        pass
+        image_file_name = self.extract_image()
+        img_2 = self.resizing(image_file_name)
+        prediction_number = self.read_predict_image(img_2)
+        self.update_prediction(prediction_number)
+        
+    def extract_image(self):
+        # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
+        filename = "img.jpg"
+        self.image1.save(filename)
+        return "img.jpg"
+    
     def reset_button_pushed(self): #Called when reset pushed
         self.canvas.delete('all')
+        self.image1 = Image.new("RGB", (600, 600), (255, 255, 255))
+        self.carbonCopy = ImageDraw.Draw(self.image1)
         #self.canvas.create_image()
+
+    def create_label(self):
+        self.prediction_label = tk.Label(root, text= "Prediction number :" , font="Arial 24", bg="green",height = 3)
+        self.prediction_label.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+
+    def update_prediction(self, prediction_number):
+        self.prediction_label.config(text = f"Prediction number: " + str(prediction_number))
     def create_buttons(self):
         self.ok_button = tk.Button(root, text='OK', font='Arial 24', \
-                              bg='Yellow', width=15, command = self.ok_button_pushed)
+                              bg='Yellow', width=15, padx=5 , pady=5,command = self.ok_button_pushed)
         self.reset_button = tk.Button(root, text='Reset', font='Arial 24', \
-                                 bg='Yellow', width=15, command = self.reset_button_pushed)
-
+                                 bg='Yellow', width=15,padx=5 , pady=5, command = self.reset_button_pushed)
         self.ok_button.grid(row=1, column=0, sticky="nsew")
         self.reset_button.grid(row=1, column=1, sticky="nsew")
 
-        # self.canvas_rows, self.canvas_columns = 2, 2
-        # for i in range(self.canvas_rows):
-        #     root.rowconfigure(root, i, weight = 1)
-        # for j in range(self.canvas_columns):
-        #     root.columnconfigure(root, j, weight = 1)
-
     def create_canvas(self):
-        self.canvas = tk.Canvas(self.root, width = 800, height = 400)
+        self.canvas = tk.Canvas(self.root, width = 600, height = 600)
         self.canvas.grid(row = 0, column = 0, columnspan = 2)
         self.canvas.bind('<Button-1>', self.start_draw)
         self.canvas.bind('<B1-Motion>', self.draw_line)
+        
     def start_draw(self, event):
         self.lastx, self.lasty = event.x, event.y
+        
     def draw_line(self, event):
         self.canvas.create_line((self.lastx, self.lasty, event.x, event.y), width = 20)
+
+        black = (0, 0, 0)
+        self.carbonCopy.line([self.lastx, self.lasty, event.x, event.y], black, width = 20)
         self.lastx, self.lasty = event.x, event.y
 
     #define baseline model
     def baseline_model(self):
         # create model
-        model = Sequential()
-        model.add(Dense(num_pixels, input_shape=(num_pixels,), kernel_initializer='normal', activation='relu'))
-        model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+        model1 = Sequential()
+        model1.add(Dense(self.num_pixels, input_shape=(self.num_pixels,), kernel_initializer='normal', activation='relu'))
+        model1.add(Dense(self.num_classes, kernel_initializer='normal', activation='softmax'))
         # Compile model
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        return model
-    
-    def train__test_model(self):
+        model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model1
+
+    def train_test_model(self):
         # load data
         (X_train, y_train), (X_test, y_test) = mnist.load_data()
         # flatten 28*28 images to a 784 vector for each image
-        num_pixels = X_train.shape[1] * X_train.shape[2]
-        X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
-        X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+        self.num_pixels = X_train.shape[1] * X_train.shape[2]
+        X_train = X_train.reshape((X_train.shape[0], self.num_pixels)).astype('float32')
+        X_test = X_test.reshape((X_test.shape[0], self.num_pixels)).astype('float32')
         # normalize inputs from 0-255 to 0-1
         X_train = X_train / 255
         X_test = X_test / 255
         # one hot encode outputs
         y_train = to_categorical(y_train)
         y_test = to_categorical(y_test)
-        num_classes = y_test.shape[1]
+        self.num_classes = y_test.shape[1]
         # build the model
-        model = baseline_model()
+        self.model = self.baseline_model()
         # Fit the model
-        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+        self.model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=200, verbose=2)
         # Final evaluation of the model
-        scores = model.evaluate(X_test, y_test, verbose=0)
+        scores = self.model.evaluate(X_test, y_test, verbose=0)
         print("Baseline Error: %.2f%%" % (100 - scores[1] * 100))
 
-    def read_predict_image(self):
-        image_number = 1
+    def read_predict_image(self, img_2):
+        if not os.path.isfile(img_2):
+            print('Error')
+            return
+
+        img = cv2.imread(img_2)[:,:,0]
+        img = np.invert(np.array([img]))
+        img = np.reshape(img, (1, 28 * 28))
         
-        while os.path.isfile(f"digits/digit{image_number}.png"):
-            img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
-            img = np.invert(np.array([img]))
+        prediction = self.model.predict(img)
+        print(f"This is probably a {np.argmax(prediction)}")
+        return np.argmax(prediction)
 
-            prediction = model.predict(img)
-            print(f"This is propably a {np.argmax(prediction)}")
+    def resizing(self,image_file_name):
+        image = Image.open(image_file_name)
+        image_file_name_2 = "image_file_name_2.jpeg"
+        
+        new_image = image.resize((28 , 28))
+        new_image.save(image_file_name_2)
+        return image_file_name_2
 
-            plt.imshow(img[0], cmap= plt.cm.binary)
-            plt.show()
-            image_number += 1
 
 
 root = tk.Tk()
